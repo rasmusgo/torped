2480f28094f009c3d36e7c013bf4efc0f82b3758
Rasmus Göransson <rasmus@volumental.com>
2016-12-22 21:50:44 +0100

    Log to file

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,10 @@
 
 int main(int argc, char *argv[])
 {
+    loguru::add_file("logs/info.log", loguru::FileMode::Truncate, loguru::Verbosity_INFO);
+    loguru::add_file("logs/full.log", loguru::FileMode::Truncate, loguru::Verbosity_MAX);
     loguru::add_callback("console", App::Console::LogHandler, &App::console, loguru::Verbosity_MAX);
-    loguru::init(argc, argv);
+    loguru::init(argc, argv); // Log time of start and set verbosity level.
 
 	App::InitAll(argc, argv);
 	atexit(App::QuitAll);
