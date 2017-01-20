b35192528c052013b4d0f815ae572d1f2ba5c10a
Rasmus Göransson <rasmus@volumental.com>
2017-01-20 20:12:00 +0100

    Remove python from CMakeLists.txt, .travis.yml and build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install system libs with:
-# sudo apt-get install libopenal-dev libalut-dev freeglut3-dev libglew-dev glew-utils liblua5.1-dev libphysfs-dev python-dev libtinyxml-dev
+# sudo apt-get install libopenal-dev libalut-dev freeglut3-dev libglew-dev glew-utils liblua5.1-dev libphysfs-dev libtinyxml-dev
 
 set -e # exit on error
 
