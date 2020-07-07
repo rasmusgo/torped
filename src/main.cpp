e970b34de5b1d4aa00a26f5e910051fedf1b80c3
Rasmus Göransson <rasmus@volumental.com>
2020-07-07 21:02:30 +0200

    main.cpp: Workaround for windows specific build error

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,12 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900)
+#include <stdio.h>
+FILE _iob[] = {*stdin, *stdout, *stderr};
+extern "C" FILE * __cdecl __iob_func(void)
+{
+    return _iob;
+}
+#endif
