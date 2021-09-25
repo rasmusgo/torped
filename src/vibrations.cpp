7915ab2fcbc8fc336bc0c4019d09a0072d2bcdad
Rasmus Göransson <rasmus@volumental.com>
2021-09-25 12:58:49 +0200

    Handle constant terms in system of differential equations
    This is done by expanding the state vector with a constant value 1
    instead of making the system non-homogenous.

--- src/vibrations.cpp
+++ src/vibrations.cpp
@@ -4,9 +4,12 @@
 
 #include <Eigen/Eigen>
 #include <unsupported/Eigen/AutoDiff>
+#include <unsupported/Eigen/MatrixFunctions>
 
 #include "physics.hpp"
 #include "logging.hpp"
+
+static const double TAU = 3.14159265358979323846264338327950288 * 2.0;
 
 using Jet12 = Eigen::AutoDiffScalar<Eigen::Vector<double, 12>>; // x0, y0, z0, x0', y0', z0', x1, y1, z1, x1', y1', z1'
 
@@ -29,17 +32,25 @@ Eigen::Vector<Jet12, 3> SpringForce(
 void computeVibrations()
 {
     // Input: Two point masses connected by a single spring.
-    // F = k*u + d*u'  spring with damping, u is length displacement of spring, k is spring constand, d is damping.
+    // F = k*u + d*u'  spring with damping, u is length displacement of spring, k is spring constant, d is damping.
     // F = m*a
     // Linearize spring equation wrt. x,y,z and their derivatives.
     // X' = AX + g(t)  where X = [x0, y0, z0, x0', y0', z0', x1, y1, z1, x1', y1', z1', ...]
     // A is a sparse square matrix containing the coefficients of the linear equations.
+    // The solution to the homogenous part X' = AX is X(t) = exp(At) * X(0)
+    // exp(At) can be computed by eigen decomposition: A = V*D*V^-1 => exp(At) = exp(V*D*V^-1*t) = V*exp(D*t)*V^-1
+
+
     // Compute the complex eigenvalues λ and eigenvectors η of A to get the general solution.
     // (Some extra work is needed to handle repeated eigenvalues)
     // X(t) = c1 * exp(λ1*t) * η1 + c2 * exp(λ2*t) * η2 + ...
     // Where c1, c2, etc are real valued constants, λ1, λ2 are complex eigenvalues and
     // η1, η2 are their respective eigenvectors.
     // Apply the initial conditions to solve for the constants and get the actual solution.
+    // X(0) = c1 * exp(0) * η1 + c2 * exp(0) * η2 + ...
+    // X(0) = c1 * η1 + c2 * η2 + ...
+    // X(0) = [η1, η2, ...] * c
+    // [η1, η2, ...]^-1 * X(0) = c
 
     // Output: Coefficients for a function of positions over time on the form:
     //   f(t) = c0 + c1 * t + c2 * t^2 + exp(c3 * t) * (c5 * cos(c4 * t) + c6 * sin(c4 * t))
@@ -51,8 +62,13 @@ void computeVibrations()
 
     const size_t num_points = 2;
     const size_t num_springs = 1;
+    const size_t N = 6 * num_points + 1;
     // SparseMatrix<double> A(6 * num_points, 6 * num_points);
-    MatrixXd A = MatrixXd::Zero(6 * num_points, 6 * num_points);
+    MatrixXd A = MatrixXd::Zero(N, N);
+    VectorXd X0 = VectorXd::Zero(N);
+    X0(0) = 0.5;
+    X0(6) = 1.5;
+    X0(6 * num_points) = 1.0;
 
     // Equations for variable substitutions
     for (int i = 0; i < num_points; ++i)
@@ -91,23 +107,30 @@ void computeVibrations()
         const int point_a_start_index = i * 6;
         const int point_b_start_index = (i + 1) * 6;
 
-        A_pos(0).value() = 0.5;
-        B_pos(0).value() = 1.5;
+        for (int j = 0; j < 3; ++j)
+        {
+            A_pos(j).value() = X0(point_a_start_index + j);
+            A_vel(j).value() = X0(point_a_start_index + j + 3);
+            B_pos(j).value() = X0(point_b_start_index + j);
+            B_vel(j).value() = X0(point_b_start_index + j + 3);
+        }
         const double relaxed_length = 0.5;
-        const double k = 100;
-        const double d = 10;
-        const double point_a_mass = 1.0;
-        const double point_b_mass = 0.2;
+        const double k = 10000;
+        const double d = 0.04;
+        const double point_a_mass = 10.0e-3;
+        const double point_b_mass = 2.0e-3;
 
         const Vector<Jet12, 3> force = SpringForce(A_pos, A_vel, B_pos, B_vel, relaxed_length, k, d);
 
         for (int j = 0; j < 3; ++j)
         {
             // Acceleration is force divided by mass. Force is defined by the spring equation.
-            A.block<1, 6>(point_a_start_index + 3 + j, point_a_start_index) += force(j).derivatives().topRows<6>().transpose() / point_a_mass;
-            A.block<1, 6>(point_a_start_index + 3 + j, point_b_start_index) += force(j).derivatives().bottomRows<6>().transpose() / point_a_mass;
-            A.block<1, 6>(point_b_start_index + 3 + j, point_a_start_index) -= force(j).derivatives().topRows<6>().transpose() / point_b_mass;
-            A.block<1, 6>(point_b_start_index + 3 + j, point_b_start_index) -= force(j).derivatives().bottomRows<6>().transpose() / point_b_mass;
+            A.block<1, 6>(point_a_start_index + 3 + j, point_a_start_index) -= force(j).derivatives().topRows<6>().transpose() / point_a_mass;
+            A.block<1, 6>(point_a_start_index + 3 + j, point_b_start_index) -= force(j).derivatives().bottomRows<6>().transpose() / point_a_mass;
+            A.block<1, 6>(point_b_start_index + 3 + j, point_a_start_index) += force(j).derivatives().topRows<6>().transpose() / point_b_mass;
+            A.block<1, 6>(point_b_start_index + 3 + j, point_b_start_index) += force(j).derivatives().bottomRows<6>().transpose() / point_b_mass;
+            A.coeffRef(point_a_start_index + 3 + j, 6 * num_points) -= force(j).value() / point_a_mass;
+            A.coeffRef(point_b_start_index + 3 + j, 6 * num_points) += force(j).value() / point_b_mass;
         }
     }
 
@@ -127,15 +150,38 @@ void computeVibrations()
     LOG_S(INFO) << "Here is the matrix A:\n" << A;
 
     EigenSolver<MatrixXd> es(A);
-    LOG_S(INFO) << "The eigenvalues of A are:\n" << es.eigenvalues();
-    LOG_S(INFO) << "The matrix of eigenvectors, V, is:\n" << es.eigenvectors();
-    complex<double> lambda = es.eigenvalues()[0];
-    LOG_S(INFO) << "Consider the first eigenvalue, lambda = " << lambda;
-    VectorXcd v = es.eigenvectors().col(0);
-    LOG_S(INFO) << "If v is the corresponding eigenvector, then lambda * v = \n" << lambda * v;
-    LOG_S(INFO) << "... and A * v = \n" << A.cast<complex<double> >() * v;
 
+    // MatrixXd D = es.pseudoEigenvalueMatrix();
+    // MatrixXd V = es.pseudoEigenvectors();
     MatrixXcd D = es.eigenvalues().asDiagonal();
     MatrixXcd V = es.eigenvectors();
-    LOG_S(INFO) << "Finally, V * D * V^(-1) = \n" << V * D * V.inverse();
+    LOG_S(INFO) << "The eigenvalues of A are:\n" << D;
+    LOG_S(INFO) << "The matrix of eigenvectors, V, is:\n" << V;
+    LOG_S(INFO) << "V \\ X0 is:\n" << V.inverse() * X0;
+    for (int i = 0; i < es.eigenvalues().rows(); ++i)
+    {
+        if (es.eigenvalues()(i).imag() != 0)
+        {
+            double frequency = std::abs(es.eigenvalues()(i).imag()) / TAU;
+            LOG_S(INFO) << "Found vibrational mode with frequency: " << frequency << " hz:\n" << V.col(i);
+        }
+    }
+
+    // complex<double> lambda = es.eigenvalues()[0];
+    // LOG_S(INFO) << "Consider the first eigenvalue, lambda = " << lambda;
+    // VectorXcd v = es.eigenvectors().col(0);
+    // LOG_S(INFO) << "If v is the corresponding eigenvector, then lambda * v = \n" << lambda * v;
+    // LOG_S(INFO) << "... and A * v = \n" << A.cast<complex<double> >() * v;
+
+    // LOG_S(INFO) << "V * V^(-1) = \n" << round((V * V.inverse() * 1.0e5).array()) * 1.0e-5;
+    // LOG_S(INFO) << "V * D * V^(-1) = \n" << round((V * D * V.inverse() * 1.0e5).array()) * 1.0e-5;
+    // LOG_S(INFO) << "V^(-1) * A * V = \n" << round((V.inverse() * A * V * 1.0e5).array()) * 1.0e-5;
+    LOG_S(INFO) << "V * V^(-1) = \n" << V * V.inverse();
+    LOG_S(INFO) << "V * D * V^(-1) = \n" << V * D * V.inverse();
+    LOG_S(INFO) << "V^(-1) * A * V = \n" << V.inverse() * A * V;
+
+    MatrixXd expA = A.exp();
+    LOG_S(INFO) << "exp(A) = \n" << expA;
+    LOG_S(INFO) << "X0 = \n" << X0;
+    LOG_S(INFO) << "exp(A)*X0 = \n" << expA * X0;
 }
